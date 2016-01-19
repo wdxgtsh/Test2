030313c0b92f844a433a1d2a9825e4ee2d05f6cc
zhaolei <zhaolei@allinmd.cn>
2016-01-19 15:15:42 +0800

    add twice

---
+++ Test_project_1/Test_project_1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Test_project_1
+//
+//  Created by zhaolei on 16/1/19.
+//  Copyright © 2016年 zhaolei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Test_project_1/Test_project_1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Test_project_1
+//
+//  Created by zhaolei on 16/1/19.
+//  Copyright © 2016年 zhaolei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
